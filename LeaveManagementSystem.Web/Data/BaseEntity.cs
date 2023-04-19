edb0a769bff112d46b3ac3f6095430599ad92304
Mario Velazquez <mario.velazquez@glennonbrothers.ie>
2023-04-19 16:35:34 +0100

    Added the Employee class on the Login Partial

--- LeaveManagementSystem.Web/Data/BaseEntity.cs
+++ LeaveManagementSystem.Web/Data/BaseEntity.cs
@@ -1,6 +1,10 @@
 ﻿namespace LeaveManagementSystem.Web.Data
 {
-	public class BaseEntity
+	//Partial Meand that you can not use this Class on its own you have to extended a class using this class
+	public abstract class BaseEntity
 	{
+		public int Id { get; set; }
+		public DateTime DateCreated { get; set; }
+		public DateTime DateModified { get; set; }
 	}
 }
--- LeaveManagementSystem.Web/Data/LeaveAllocation.cs
+++ LeaveManagementSystem.Web/Data/LeaveAllocation.cs
@@ -2,16 +2,15 @@
 
 namespace LeaveManagementSystem.Web.Data
 {
-	public class LeaveAllocation
+	public class LeaveAllocation:BaseEntity
 	{
-        public int Id { get; set; }
+       
 	    public int NumberOfDays { get; set; }
 		[ForeignKey("LeaveTypeId")]
 		public LeaveType LeaveType { get; set; }
 		public int LeaveTypeId { get; set; }
 		public string EmployeeId { get; set; }
-		public DateTime DateCreated { get; set; }
-		public DateTime DateModified { get; set; }
+		
 
 	}
 }
